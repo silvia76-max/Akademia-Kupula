b07199eec59a61fbc49d74a0586010c3d58333f6
silvia76-max <silviarodriguezexposito.76@gmail.com>
2024-12-13 21:10:42 +0100

    nuevos cambios en index boton y formulario

--- script.js/script.js
+++ script.js/script.js
@@ -1,4 +1,17 @@
- 
+document.addEventListener("DOMContentLoaded", () => {
+  const loginButton = document.getElementById("login-button");
+  const loginForm = document.getElementById("login-form");
+
+  loginButton.addEventListener("click", () => {
+      loginForm.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", (event) => {
+      if (!loginForm.contains(event.target) && !loginButton.contains(event.target)) {
+          loginForm.classList.add("hidden");
+      }
+  });
+}); 
  
    document.getElementById('toggle-button').addEventListener('click', function () {
     const formulario = document.getElementById('formulario-registro');
