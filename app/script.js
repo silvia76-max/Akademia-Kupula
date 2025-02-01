7d8905209686786ed09656504d41cd51495d3bce
silvia76-max <silviarodriguezexposito.76@gmail.com>
2025-02-01 19:12:34 +0100

    cambios en estilos iconos

--- app/script.js
+++ app/script.js
@@ -7,19 +7,4 @@ hamburgerButton.addEventListener('click', () => {
 });
 
 
-// JavaScript para manejar el despliegue del formulario
-
-const toggleButton = document.getElementById('toggleButton'); // Botón para mostrar/ocultar formulario
-const formContainer = document.getElementById('formContainer'); // Contenedor del formulario
-
-toggleButton.addEventListener('click', () => {
-    // Verificar el estado de visibilidad del formulario
-    if (formContainer.style.display === 'none' || formContainer.style.display === '') {
-        formContainer.style.display = 'block'; // Mostrar formulario
-        toggleButton.textContent = 'Ocultar Formulario'; // Cambiar texto del botón
-    } else {
-        formContainer.style.display = 'none'; // Ocultar formulario
-        toggleButton.textContent = 'Registrate'; // Cambiar texto del botón
-    }
-});
 
